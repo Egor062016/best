2ccf0b5cda86ce69b4c21d5b0ecc0a7bfd784408
Egor062016 <107456474+Egor062016@users.noreply.github.com>
2023-03-21 18:13:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ bot = Bot('5619197827:AAERRo3fM1SwCE6l24DTTgpWwBNtFhFW8wM')
 dp = Dispatcher(bot=bot,
                 storage=storage)
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 class Conversation(StatesGroup):
     waiting_for_input = State()

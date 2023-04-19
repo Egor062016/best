c4a4bd2f67ba2fe34a03e7599f7292d579e51b66
Egor062016 <107456474+Egor062016@users.noreply.github.com>
2023-04-19 21:37:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '5866449991:AAHlHP0nQP2XOmDaX70uV-AwrXmbvt2SgjU' #Токен бота
+TOKEN = '5619197827:AAGCiBtzc9Ked8Sqfauy8_m8wPrAmR61dG0' #Токен бота
 admin = 1807653203 #Chat_id админа 1
 db = 'db.db'
